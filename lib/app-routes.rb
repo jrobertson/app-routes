bf5acdcef8c8b2b2114103ba8aa1421788ea5363
James Robertson <james@r0bertson.co.uk>
2014-02-15 18:47:50 +0000

    feature: can now handle POST requests

--- lib/app-routes.rb
+++ lib/app-routes.rb
@@ -2,24 +2,28 @@
 
 # file: app-routes.rb
 
+
 module AppRoutes
 
   attr_accessor :routes
   
   def initialize(logfile='/tmp/app-routes.log')
-    @routes = {}
+    @get_routes, @post_routes = {}, {}
     @params = {}
   end
   
-  def run_route(request)
+  def run_route(request, request_method='GET')
 
     result = nil
 
-    routes = @routes
+    return unless @get_routes or @post_routes
 
-    return unless routes
-
-    route = routes.detect {|key, block|  request.match(key) }
+    route, routes = case request_method
+      when 'GET'
+        [@get_routes.detect {|key, block|  request.match(key) }, @get_routes]
+      when 'POST'
+        [@post_routes.detect {|key, block|  request.match(key) }, @post_routes]
+    end
     
     if route then
       key, block = route      
@@ -54,19 +58,34 @@ module AppRoutes
     send (methodx[arg.class.to_s.to_sym]), arg, &block 
   end
 
-  def add_route(arg)
-    get(arg) {yield(@params)}
+  def post(arg,&block)
+    methodx = {String: :string_post, Regexp: :regex_post}
+    send (methodx[arg.class.to_s.to_sym]), arg, &block 
+  end
+
+  def add_route(arg, request_method='GET')
+    name = request_method == 'GET' ? :get : :post
+    method(name).call(arg) {yield(@params)}
   end
   
   private
 
   def string_get(raw_s, &block)
     s = "^%s$" % raw_s.gsub(/\:[\w-]+/,'([\w-]+)').gsub(/\/\*/,'(.*)')
-    @routes[Regexp.new s] = {s: raw_s, block: block}
+    @get_routes[Regexp.new s] = {s: raw_s, block: block}
   end
 
   def regex_get(r, &block)
-    @routes[r] = {block: block}
+    @get_routes[r] = {block: block}
+  end
+
+  def string_post(raw_s, &block)
+    s = "^%s$" % raw_s.gsub(/\:[\w-]+/,'([\w-]+)').gsub(/\/\*/,'(.*)')
+    @post_routes[Regexp.new s] = {s: raw_s, block: block}
+  end
+
+  def regex_post(r, &block)
+    @post_routes[r] = {block: block}
   end
 
 end
